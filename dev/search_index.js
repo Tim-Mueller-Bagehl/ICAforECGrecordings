ca46320897301921dedad26ec0a4373666ba833d
Documenter.jl <documenter@juliadocs.github.io>
2025-05-27 12:02:30 +0000

    build based on d974c9c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ICAforECGrecordings","page":"Home","title":"ICAforECGrecordings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ICAforECGrecordings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
