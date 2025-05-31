ae0d0d081e7cab304503053336e17a05e66e2913
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2025-05-31 15:58:36 +0200

    fix typo

--- src/Parser.jl
+++ src/Parser.jl
@@ -5,7 +5,7 @@ using Plots
     ReadDatasetFromDatFile(Path :: String,tabdelimited :: Bool = true)
 
 Reads .dat File from a File Location. 
-returns 9x2497 Matrix {Float64} with oure dataset
+returns 9x2497 Matrix {Float64} with our dataset
 """
 function ReadDatasetFromDatFile(Path :: String)
     return readdlm(Path,Float64)
