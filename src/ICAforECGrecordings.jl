5f9c6ddf83e280f7170ef0237cdf99cedaed9a88
soybean000 <63549213+soybean000@users.noreply.github.com>
2025-06-07 15:22:23 +0200

    add whitening function

--- src/ICAforECGrecordings.jl
+++ src/ICAforECGrecordings.jl
@@ -7,4 +7,29 @@ include("functions.jl")
 export ReadDatasetFromDatFile
 include("Parser.jl")
 
+export whiten
+
+"""
+whiten(data::Matrix{Float64}) -> Matrix{Float64}
+
+Performs whitening on the given data matrix.
+
+Whitening transforms the input signals so that each signal has unit variance and all signals are uncorrelated.
+This is a common preprocessing step before applying Independent Component Analysis (ICA).
+
+# Arguments
+- `data`: A matrix of size (n_samples, n_signals) where each column is a signal.
+
+# Returns
+- A whitened data matrix of the same size, where the signals are zero-mean, uncorrelated, and have unit variance.
+
+"""
+function whiten(data::Matrix{Float64})
+    centered = data .- mean(data, dims=1)
+    cov_matrix = cov(centered, dims=1)
+    eigvals, eigvecs = eigen(cov_matrix)
+    whitening_matrix = inv(sqrt.(Diagonal(eigvals))) * eigvecs'
+    whitened = centered * whitening_matrix'
+    return whitened
+end
 end
