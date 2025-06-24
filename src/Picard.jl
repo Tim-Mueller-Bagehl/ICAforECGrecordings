3d894a85e5bc7850e59a30752a229d2c43ea25cc
Jan <j.buettner@campus.tu-berlin.de>
2025-06-24 17:31:42 +0200

    Picard main fuction implementet

--- src/Picard.jl
+++ src/Picard.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+using Infinity
 """
     picard(X::Matrix{Float64}, m::Int, maxiter::Int, tol::Float32, lambda_min::Float32, ls_tries, verbose::Bool)
 Runs the Picard algorithm.
@@ -12,4 +14,76 @@ Runs the Picard algorithm.
 
 """
 function picard(X::Matrix{Float64}, m::Int, maxiter::Int, tol::Float32, lambda_min::Float32, ls_tries, verbose::Bool)
+
+    N, T = size(X) 
+
+    W = I(N)
+    Y = X
+
+    s_list = zeros(0)
+    y_list = zeros(0)
+    r_list = zeros(0)
+    current_loss = ∞
+    sign_change = false
+
+    for i in maxiter
+        psiY = score(Y)
+        psidY_mean = score_der(psiY)
+
+        g = gradient(Y, psiY)
+
+        K = psidY_mean - diagm(g)
+        signs = sign.(K)
+        if n > 1
+            sign_change = signs ≠ old_signs
+        end
+        old_signs = signs
+
+        psidY_mean = psidY_mean .* signs
+
+        G .= (g .- transpose(g)) ./ 2.0
+
+        gradient_norm = max(max(abs.(G)))
+        if gradient_norm < tol
+            break
+        end
+
+        if n > 1
+            append!(s_list, direction)
+            y = G .- G_old
+            append!(y_list, y)
+            append!(r_list, 1. / sum(sum(direction .* y)))
+            if length(s_list) > m
+                deleteat!(s_list,1)
+                deleteat!(y_list,1)
+                deleteat!(r_list,1)
+            end
+        end
+        G_old = G
+        if sign_change
+            s_list = zeros(0)
+            y_list = zeros(0)
+            r_list = zeros(0)
+            current_loss = ∞
+        end
+        h = proj_hessian_approx(Y, psidY_mean, g)
+        h = regularize_hessian(h, lambda_min)
+        converged, new_Y, new_loss, alpha = line_search(Y, signs, direction, current_loss, ls_tries)
+
+        if !converged
+            direction = -G
+            s_list = zeros(0)
+            y_list = zeros(0)
+            r_list = zeros(0)
+            tmp, new_Y, new_loss, alpha = line_search(Y, signs, direction, current_loss, ls_tries)
+        end
+        direction = alpha * direction
+        Y = new_Y
+        W = expm(direction) .* W
+        current_loss = new_loss
+        if verbose
+            @info iteration n, gradient norm = gradient_norm
+        end
+    end
+
 end
