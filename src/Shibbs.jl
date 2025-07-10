260e20e6618be26f4e4cdef9f3c83cedb43c37de
Jan <j.buettner@campus.tu-berlin.de>
2025-07-10 18:13:20 +0200

    Fixxed Shibbs  number of signals

--- src/Shibbs.jl
+++ src/Shibbs.jl
@@ -7,20 +7,21 @@ Returns
 CM=cumulant_matrices(X) a NxN*nbcm cumulant matrix.
 
 """
-function cumulant_matrices(X::AbstractMatrix)
+function cumulant_matrices(X::AbstractMatrix, m::Number)
     N, T = size(X)
-    CM = zeros(N, N*N)
-    Rk = zeros(N, N)
-    R = I(N)
-    Xk = zeros(N, T)  
+    CM = zeros(m, m*m)
+    Rk = zeros(m, m)
+    R = I(m)
+    Uns = ones(Int, m)  
+    Xk = zeros(m, T)  
     xk = zeros(1, T) 
     
-    for k in 1:N
+    for k in 1:m
         xk = X[k, :]
-        Xk = X .* xk'
+        Xk = X .* xk[Uns, :]  
         Rk = Xk * Xk' / T - R
         Rk[k, k] -= 2
-        CM[:, ((k - 1) * N + 1):(k * N)] .= Rk
+        CM[:, ((k - 1) * m + 1):(k * N)] .= Rk
     end
 
     return CM
@@ -35,13 +36,13 @@ Returns
 V = joint_diagonalization(CM, threshhold) a NxN diagonalized matrix.
 
 """
-function joint_diagonalization(CM::AbstractMatrix, threshhold::Real)
-    N, _ = size(CM)
-    V = Matrix{Float64}(I, N, N)
+function joint_diagonalization(CM::AbstractMatrix, threshhold::Real, m::Number)
+    
+    V = Matrix{Float64}(I, m, m)
     nbrs = 1   
     sweep = 0    
     updates = 0 
-    g	= zeros(2, N)
+    g	= zeros(2, m)
     gg	= zeros(2, 2)
     G	= zeros(2, 2)
     c = 0.0
@@ -54,10 +55,10 @@ function joint_diagonalization(CM::AbstractMatrix, threshhold::Real)
     while repeat && sweep < 5000
         sweep += 1
         nbrs = 0
-        for p = 1:N-1
-            for q = (p+1):N
-                Ip = p:N:N*N 
-	            Iq = q:N:N*N
+        for p = 1:m-1
+            for q = (p+1):m
+                Ip = p:m:m*m 
+	            Iq = q:m:m*m
 	            g = [CM[p,Ip] .- CM[q,Iq]; CM[p,Iq] .+ CM[q,Ip]]
 	            gg	= g * g' 
 	            ton = gg[1,1] - gg[2,2]
@@ -94,15 +95,30 @@ B=shibbsR(X) a nxn separating matrix such that S=B*X is an nxT
 matrix of estimated source signals.
 
 """
-function shibbs(X::Matrix{Float64})
+function shibbs(X::Matrix{Float64},m=nothing)
+
     signals = X[:, 2:end]
     X = Matrix(signals')
+    N, T = size(X)   
+    m = isnothing(m) ? N : m
 
-    N, T = size(X)         
     threshhold = 0.01 / sqrt(T)
     MeanX   = mean(X, dims=2)
     X .-= MeanX 
-    B = I(N)
+    C = (X * X') / T                    
+    eig = eigen(C)                      
+
+    D = eig.values                      
+    U = eig.vectors                     
+
+    k = sortperm(D)                         
+    puiss = D[k]                        
+
+    rangeW = N-m+1:N              
+    scales = sqrt.(puiss[rangeW])       
+
+    B = Diagonal(1.0 ./ scales) * U[:, k[rangeW]]'
+    X = B * X  
     
     OneMoreStep = true
     outersweep = 0
@@ -115,16 +131,16 @@ function shibbs(X::Matrix{Float64})
             @info "outer loop done: " percent
         end
 
-        CM = cumulant_matrices(X) # calculate cumulant matrices
+        CM = cumulant_matrices(X,m) # calculate cumulant matrices
 
-        V = joint_diagonalization(CM, threshhold) # calculate diagonalization matrices
+        V = joint_diagonalization(CM, threshhold,m) # calculate diagonalization matrices
 
-        RotSize = norm(V - I(N), 2) # calculate rotation size
+        RotSize = norm(V - I(m), 2) # calculate rotation size
 
         X = V' * X # Update X
         B = V' * B # Update B
 
-        if RotSize < (N * threshhold) # check convergence
+        if RotSize < (m * threshhold) # check convergence
             OneMoreStep = false
         end  
     end
@@ -133,7 +149,7 @@ function shibbs(X::Matrix{Float64})
     A = pinv(B)
     vars = sum(A .* A, dims=2)
     keys = sortperm(vec(vars))
-    B = B[keys, :]
+    B = B[:, keys]
     # fix signs of B to be non negativ 
     b = B[:, 1]
     signs = sign.(b)
