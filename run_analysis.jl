15af7bd69e8c1613cee24220c83ffe150488b5ee
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2025-07-11 13:47:36 +0200

    add more test cases visualization

--- run_analysis.jl
+++ run_analysis.jl
@@ -1,11 +1,11 @@
 using Pkg
-Pkg.activate(temp=true)
-Pkg.add(url="https://github.com/Tim-Mueller-Bagehl/ICAforECGrecordings")
+# Pkg.activate(temp=true)
+# Pkg.add(url="https://github.com/Tim-Mueller-Bagehl/ICAforECGrecordings")
+Pkg.activate(@__DIR__)
 Pkg.resolve()
 Pkg.instantiate()
 Pkg.add("Plots")
 import ICAforECGrecordings
-using ICAforECGrecordings: whiten, plot_dataset, read_dataset_from_dat, solve, JadeSeperator, ShibbsSeperator
 using ICAforECGrecordings: whiten, plot_dataset, read_dataset_from_dat, solve, JadeSeperator, ShibbsSeperator
 using Plots: savefig
 
--- src/Visualization.jl
+++ src/Visualization.jl
@@ -5,9 +5,13 @@ Plot a dataset with time on the x-axis and multiple signals on the y-axis.
 - `data::AbstractMatrix{Float64}`: Matrix where the first column represents time and the remaining columns represent different signals.
 """
 function plot_dataset(data::AbstractMatrix{Float64})
+    if isempty(data)
+        @warn "Input data is empty. Returning nothing."
+        return nothing
+    end
     time = data[:, 1]
     signals = data[:, 2:end]
-    n_signals = size(signals, 2)
+    n_signals = size(signals)
 
     plt = plot(size=(1600, 400), legend=:topright, title="ECG Signals Over Time",
                xlabel="Time", ylabel="Voltage", grid=true, background_color=:white)
