8b4ffad9c836bc1cc2305d8ea1493311f5b8f9e9
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2025-07-11 13:35:28 +0200

    rewrite plot_dataset

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -9,13 +9,13 @@ function plot_dataset(data::AbstractMatrix{Float64})
     signals = data[:, 2:end]
     n_signals = size(signals, 2)
 
-    plt = plot(layout=(n_signals, 1), link=:x, size=(800, 200 * n_signals))
+    plt = plot(size=(1600, 400), legend=:topright, title="ECG Signals Over Time",
+               xlabel="Time", ylabel="Voltage", grid=true, background_color=:white)
 
     for i in 1:n_signals
-        plot!(plt[i], time, signals[:, i], label="Signal $i", ylabel="Value")
-        if i == n_signals
-            xlabel!(plt[i], "Time")
-        end
+        plot!(plt, time, signals[:, i], label="Signal $i", lw=3)
     end
+
+
     return plt
 end
