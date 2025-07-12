c3e7f3131a8840e33d547f5d6150efd019b88106
Johann <9853102+hajoha@users.noreply.github.com>
2025-07-12 17:43:01 +0200

    update (#48)

--- test/test_visualization.jl
+++ test/test_visualization.jl
@@ -8,4 +8,24 @@ using Test: @testset, @test, @test_throws
         plt = ICAforECGrecordings.plot_dataset(data) 
         @test !isnothing(plt)
     end
+
+    @testset "plot_dataset with empty data" begin
+        data = zeros(Float64, 0, 2) 
+        plt = ICAforECGrecordings.plot_dataset(data)
+        @test plt === nothing
+    end
+
+    @testset "plot_dataset with two signals" begin
+        data = [0.0 1.0 2.0; 1.0 1.5 2.5; 2.0 2.0 3.0; 3.0 2.5 3.5]
+        plt = ICAforECGrecordings.plot_dataset(data)
+        @test !isnothing(plt)
+    end
+    @testset "plot_dataset with multiple signals" begin
+        data = [0.0 1.0 2.0 3.0; 1.0 1.5 2.5 3.5; 2.0 2.0 3.0 4.0; 3.0 2.5 3.5 4.5]
+        plt = ICAforECGrecordings.plot_dataset(data)
+        @test !isnothing(plt)
+    end
+
+
+
 end
