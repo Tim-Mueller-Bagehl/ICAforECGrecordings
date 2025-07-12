8ef6f124c530303bbed8029a0c347bb6f6e82ccc
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2025-07-12 16:45:04 +0200

    update

--- run_analysis.jl
+++ run_analysis.jl
@@ -1,11 +1,10 @@
 using Pkg
-Pkg.activate(temp=true)
-Pkg.add(url="https://github.com/Tim-Mueller-Bagehl/ICAforECGrecordings")
+Pkg.activate(".")
+Pkg.add("ICAforECGrecordings")
 Pkg.resolve()
 Pkg.instantiate()
 Pkg.add("Plots")
 import ICAforECGrecordings
-using ICAforECGrecordings: whiten, plot_dataset, read_dataset_from_dat, solve, JadeSeperator, ShibbsSeperator
 using ICAforECGrecordings: whiten, plot_dataset, read_dataset_from_dat, solve, JadeSeperator, ShibbsSeperator
 using Plots: savefig
 
