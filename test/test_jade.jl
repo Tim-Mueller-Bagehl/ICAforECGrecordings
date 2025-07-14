1f21d646e7866c0d34a383321833edbb0fcb8014
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2025-07-14 21:06:35 +0200

    add jade test

--- test/test_jade.jl
+++ test/test_jade.jl
@@ -1,39 +1,16 @@
 using ICAforECGrecordings
 using Test
-using Random
-using LinearAlgebra: adjoint, transpose, isapprox, randn
-using Statistics: cor
-
-#=
 @testset "jade function" begin
-    Random.seed!(123)
-
-    m = 4
-    n = 1000
-    time = (collect(1:n))
-    freq1, freq2 = 0.05, 0.07
-
-    s1 = sin.(2*pi * freq1 * time)
-    s2 = sin.(2*pi * freq2 * time)
-    sources = [s1'; s2']
-
-    W = randn(m, m)
-
-    X = W * vcat(sources, randn(m-2, n))
     
-    data_w = hcat(time, X')
-    output = jade(data_w, W)
+    time = 0:pi/100:4*pi
+    noise = rand(length(time)) .- 0.5   
+    signal = sin.(time) 
+    square_wave = squarewave1.(time)
 
-    # Test the size of return value
-    @test size(output) == (n, 3)
-    # Test the time column
-    @test isapprox(output[:,1], time)
+    data = hcat(time, signal, noise)
+    signals = solve(JadeSeperator(), data)
+    @test size(signals) == size(data)
+    @test isapprox(signals[:, 2], signal, atol=1e-5)
+    @test isapprox(signals[:, 3], noise, atol=1e-5)
 
-    # Test whether signals are Independent
-    recovered = output[:, 2:3]
-    norms = norm.(eachcol(recovered))
-    @test all(norms .> 0.1)
-    corr = cor(recovered[:, 1], recovered[:, 2])
-    @test abs(corr) < 0.3
 end
-=#
